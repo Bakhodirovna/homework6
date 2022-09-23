42ca4cca2be6db910d45eef6de4b3bd743781c7f
Zahro <zahro6800@gmail.com>
2022-09-23 14:56:11 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+// // =====basic=====
+
+
+// // 2
+// function myFunction(a, b) {
+// if(a === b){
+// return true
+// }else{
+//     return false
+// }
+// }
+
+// // 3
+
+// function myFunction(a, n) {
+// return a[n-1]
+// }
+
+// // 4
+    
+// function myFunction(a) {
+//     return typeof a;
+// }
+
+// // 5
+// Write a function that takes a string (a) as argument
+// Remove the first 3 characters of a
+// Return the result
+
+// function myFunction(a) {
+//     return a.slice(0,3)
+// }
+
+
+// //6
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+
+
+
+
+// // ====random color=====
+
+// const colors = ["red","green","yellow","blue","black","purple","lightpink"]
+
+// const random = colors[Math.floor(Math.random() * colors.length)];
+
+// const body = document.body.style.backgroundColor = random;
